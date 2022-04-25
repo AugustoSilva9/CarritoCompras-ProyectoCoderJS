ced0e4a585220ed1a263425b029e80ed059e6854
Augusto Silva <augustonicolassilva@gmail.com>
2022-04-24 21:47:49 -0300

    agrego img y ultimos detalles de estilos

--- js/index.js
+++ js/index.js
@@ -1,65 +1,7 @@
-/* let codigo = document.getElementById('codigo');
-let descripcion = document.getElementById('descripcion');
-let cantidad = document.getElementById('cantidad');
-let precio = document.getElementById('precio');
-let cargar = document.getElementById('cargar');
-let seccionProd = document.getElementById('productos')
-let buscar = document.getElementById('busqueda')
-let carritoSeccion = document.getElementById('carrito')
-let nroCarrito = document.getElementById('nroCarrito')
-let iconoCarrito = document.getElementById('iconoCarrito')
-let paginaCompleta = document.getElementById('paginaCompleta')
-let carritoCompleto = document.getElementById('carritoCompleto')
-let inicio = document.getElementById('inicio')
-//crear clase producto
-let productos = [];
-let carrito = [];
-
-class Producto {
-    constructor(id, nombre, precio, cantidad, stock = true){
-        this.id = parseInt(id);
-        this.nombre = nombre.toUpperCase();
-        this.precio = parseFloat(precio);
-        this.cantidad = parseInt(cantidad);
-        this.precioIva = parseFloat(precio * 1.21);
-        this.stock = stock;
-        this.carrito = parseInt(0);
-    }
-    sumarIva() {
-        this.precioIva = (this.precio * 1.21);
-    }
-    cambiarStock(){
-        this.stock = !this.stock;
-    }
-}
-
-productos.push(new Producto(1, "Moto G60", 50000, 3));
-productos.push(new Producto(2, "A22", 45000, 6));
-productos.push(new Producto(3, "Moto G100", 89000, 0));
-productos.push(new Producto(4, "Notebook Lenovo", 195000, 1)); */
-/* let dolar = [] */
-/* function actualizarStorage(){
-    if(!localStorage.getItem("listaDeProductos")){
-        localStorage.setItem("listaDeProductos", JSON.stringify(productos))
-    } else{
-        productos = JSON.parse(localStorage.getItem("listaDeProductos"));
-    }
-}
-actualizarStorage();
-
-function carritoStorage(){
-    if(!localStorage.getItem("carrito")){
-        localStorage.setItem("carrito", JSON.stringify(carrito))
-    } else{
-        carrito = JSON.parse(localStorage.getItem("carrito"));
-        console.log(carrito)
-    }
-}
-carritoStorage(); */
 //imprimir las card de los productos
 function crearCard (producto){
     let card = document.createElement('div')
-    card.innerHTML = `<img src="http://starcomputacion.com.ar/library/timthumb/timthumb.php?src=/imagenes/productos/21229907306084a6309f807_4.jpg&w=366&h=297&zc=2" alt="">
+    card.innerHTML = `<img src="${producto.img}" alt="">
     <h3>${producto.nombre}</h3>
     <p>Precion: $${producto.precio}</p>
     <p>Precio + IVA: $${producto.precioIva}</p>
@@ -67,7 +9,7 @@ function crearCard (producto){
     <div id="cantidad-${producto.id}" class="cantidadProd oculto">
     <button type="button" id="menos-${producto.id}" class="btn btnCantidad">-</button>
     <input type="number" readonly id="cant-${producto.id}" value="1">
-    <button type="button" id="mas-${producto.id}" class="btn btnCantidad">+</button></div>`//en el input de la cantidad tendria que ir producto.carrito
+    <button type="button" id="mas-${producto.id}" class="btn btnCantidad">+</button></div>`;
     card.classList.add('card')
     seccionProd && seccionProd.append(card);
 }
@@ -81,32 +23,23 @@ function controlProductos(carrito, productos){
                 for (const el of productos) {
                 if(prod.id == el.id){
                     el.carrito = prod.carrito;
-                    console.log(prod.carrito)
-                    console.log(el.carrito)
                 }
-            }//poner una bandera que cambie si lo encuentra y modifica .carito sino lo reseteo
+            }
         }
     }
-    console.log(carrito)
-    console.log(productos)
 }
-let productosALaVista = []
-
 function imprimir(arrProductos, filtroBusqueda = true) {
     controlProductos(carrito, arrProductos)
     if(filtroBusqueda){
         productosALaVista = [];
         productosALaVista.push(...arrProductos)
     }
-    console.log(arrProductos)
-    console.log(productosALaVista)
     seccionProd.innerHTML = "";
     (arrProductos.length == productos.length) && (tituloSeccionDeProducos.innerHTML = "Productos");
     for (const producto of arrProductos) {       
         crearCard(producto)
         eventoMasMenosCantidad(producto)
         if(producto.carrito != 0){
-            console.log('prueba')
             let input = document.getElementById(`cant-${producto.id}`);
             input.value = producto.carrito;
             btnCantidad(producto.id, false)
@@ -117,24 +50,17 @@ function imprimir(arrProductos, filtroBusqueda = true) {
 // filtrar los productos que quiero ver
 if(buscar){
     buscar.addEventListener('input', (e) => {
-        //ver como limpiar el filtro si input.length = 0 filtro= []vacio
         let prodABuscar = e.target.value;
         prodABuscar = prodABuscar.toUpperCase();
         let filtro = productosALaVista.filter((prod) => prod.nombre.includes(prodABuscar));
         seccionProd.innerHTML = "";
-        console.log(filtro)
-        console.log(productos)
-        console.log(prodABuscar.length)
-        /* prodABuscar.length == 0 ? imprimir(productos) : */ imprimir(filtro, false);
-        
+        imprimir(filtro, false);
     })
     buscar.addEventListener('blur', ()=> {
         buscar.value = "";
     })
-    }
-
-
-
+}
+//traigo el valor del dolar de la API
 const valorDolar = async () => {
     const resp = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales');
     const data = await resp.json();
@@ -146,18 +72,14 @@ const valorDolar = async () => {
     }
     return valor   
 }
-
 //funciones del carrito
-
 function calcularTotal(carrito){
    let total = carrito.reduce((acc, el) => acc + (el.precioIva * el.carrito), 0)
    total = total.toFixed(2)
     return total;
 }
 async function totalEnDolares (tot){
-    console.log(tot);
     const dolar = await valorDolar();
-    console.log(dolar.replace(',','.'))
     let dolarParseado = dolar.replace(',','.')
     dolarParseado = parseFloat(dolarParseado)
     let totalUSD = (tot / parseFloat(dolarParseado));
@@ -167,25 +89,24 @@ async function totalEnDolares (tot){
 }
 
 function eliminarCarrito(prod){
-    console.log(prod)
-   const eliminarProd = carrito.findIndex((el) => el.id === prod.id)
-   carrito.splice(eliminarProd, 1)
-   prod.carrito = 0;
-   paginaCompleta.classList.contains('oculto') && imprimir(productos)
-   let input = document.getElementById(`cant-${prod.id}`);
-   input.value = prod.carrito
-   localStorage.setItem("carrito", JSON.stringify(carrito))
-   carritoStorage();
-   paginaCompleta.classList.contains('oculto') && mostrarCarrito(carrito) 
-   for (const el of productos) {
+    const eliminarProd = carrito.findIndex((el) => el.id === prod.id)
+    carrito.splice(eliminarProd, 1)
+    prod.carrito = 0;
+    paginaCompleta.classList.contains('oculto') && imprimir(productos);
+    let input = document.getElementById(`cant-${prod.id}`);
+    input.value = prod.carrito;
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    carritoStorage();
+    paginaCompleta.classList.contains('oculto') && mostrarCarrito(carrito);
+    for (const el of productos) {
        if(el.id == prod.id){
            el.carrito = 0;
        }
-   }
-   console.log(carrito)
-   console.log(prod)
-   notificacionCarrito()
-   Toastify({
+    }
+/*    console.log(carrito)
+   console.log(prod) */
+    notificacionCarrito()
+    Toastify({
     text: "tu producto se elimió del carrito",
     duration: 1500,
     gravity: 'bottom',
@@ -215,7 +136,7 @@ function imprimirCarrito (carrito){
         prodCarrito.classList.add('carritoItem')
     }
 }
-
+//muestra el total en las dos monedas
 async function totalCarrito(carrito){
     let totalC = calcularTotal(carrito)
     let total = document.createElement('div');
@@ -228,7 +149,6 @@ async function totalCarrito(carrito){
     totalDolar.innerHTML = `<h3 class="w-33">TOTAL UDS</h3><p>US$</p>
     <p id="totalDolar" class="w-33"> ${totalUDS}</p>`
     totalDolar.classList.add('totalItem') 
-    console.log(totalUDS)
     
     carritoSeccion.append(total);
     total.classList.add('carritoItem')
@@ -256,12 +176,9 @@ function cantidadCarritoModif (producto, modificacion=true){
 
 function agregarACarrito(producto){
     cantidadCarritoModif(producto)
-    console.log(producto)
     carrito.push(producto)
     localStorage.setItem("carrito", JSON.stringify(carrito))
     carritoStorage();
-    console.log(carrito)
-  //  mostrarCarrito(carrito)
 }
 
 //asigno el click a los botones y llamo a la funcion que agrega al carrito
@@ -285,32 +202,29 @@ const eventoComprar = (productos) => {
         })
     }
 }
-// funcion botones mas menos falta ver como oculto cuando dice 1 sin mostrar el 0
+// funcion botones mas menos
 function checkCantidad(prod, sumaResta) {
     let checkCantidad = carrito.find((el) => el.id === prod.id) ;
     sumaResta ? (checkCantidad.carrito++) : (checkCantidad.carrito--) ;
     let cantidad = checkCantidad.carrito;
-    console.log(cantidad)
+   /*  console.log(cantidad) */
     return cantidad
 }
 
 function masMenosCantidad ( producto, sumaResta){
-    console.log('evento mas menos')
     let input = document.getElementById(`cant-${producto.id}`);
     producto.carrito = checkCantidad(producto, sumaResta);
-    console.log(producto)
-    console.log(producto.carrito)
+/*     console.log(producto)
+    console.log(producto.carrito) */
     let cantidad = parseInt(producto.carrito);
     localStorage.setItem("carrito", JSON.stringify(carrito))
     carritoStorage();
     if(cantidad === 0){
-        //cambio el +/- por el boton comprar y elimino el elemento del carrito
         input.value = cantidad;
         btnCantidad(producto.id)
         eliminarCarrito(producto)
     }else{
         input.value = cantidad;
-        console.log(input.value + ' else')
     }
 }
 
@@ -336,15 +250,11 @@ function btnCantidad(id, cambio = true){
     
 }
 
-
-
 if(iconoCarrito){
     iconoCarrito.addEventListener('click',() => {
     mostrarCarrito(carrito)
 })
 }
-
-
 console.log(productos);
 console.log(carrito);
 
@@ -365,22 +275,16 @@ function mostrarOcultarCarrito(accion) {
         carritoCompleto.classList.add('oculto')
     }
 }
-
-inicio.addEventListener('click', () => {mostrarOcultarCarrito(false)
+inicio.addEventListener('click', () => {
+    mostrarOcultarCarrito(false)
     imprimir(productos)
 }) 
-
 //btn continuar para elegir como pagar
-let totalEnPesos = 0;
-let totalEnDolar = 0;
 continuarCompra.addEventListener('click', ()=>{ 
-    console.log('este es el boton de continuar')
     let totalPesos = document.getElementById('totalPesos').innerHTML
     totalEnPesos = parseFloat(totalPesos).toFixed(2)
     let totalDolar = document.getElementById('totalDolar').innerHTML
     totalEnDolar = parseFloat(totalDolar).toFixed(2)
-    console.log(totalEnDolar)
-    console.log(totalEnPesos)
     carritoCompleto.classList.add('oculto')
     metodoDePago.classList.remove('oculto')
 })
@@ -404,14 +308,12 @@ function mostrarMetodosPago(metodo){
         cuerpoModal.classList.add('cuerpoModal')
     }
 }
-
 //Mostrar las diferentes categorias de productos
 function mostrarCategoria(cat){
     console.log(cat)
     let categoriaAMostrar = productos.filter((el) => el.categoria == cat)
     console.log(categoriaAMostrar)
     imprimir(categoriaAMostrar)
-
 }
 //func para volver directo del carrito a una de las categorias de prod
 function mostrarCategorias(){
@@ -423,7 +325,6 @@ celulares.addEventListener('click', () => {
     mostrarCategoria(cat)
     tituloSeccionDeProducos.innerHTML = "Celulares";
 })
-
 notebook.addEventListener('click', () => {
     mostrarCategorias();
     let cat = 'NOTEBOOK'
@@ -436,39 +337,20 @@ smartwatch.addEventListener('click', () => {
     mostrarCategoria(cat)
     tituloSeccionDeProducos.innerHTML = "Smartwatch";
 })
-
-
 //mostramos el modal con los datos de la compra depende lo que hayamos elegido
-let finalCompra = document.getElementById('finalCompra')
 finalCompra.addEventListener('click', ()=> {
     let opcion = document.querySelector('input[name=formaPago]:checked');
     let metodo;
-    opcion ? metodo = opcion.value : metodo = "Debe seleccionar un metodo de pago"
-    console.log(metodo)
-    mostrarMetodosPago(metodo)
+    opcion ? metodo = opcion.value : metodo = "Debe seleccionar un metodo de pago";
+    mostrarMetodosPago(metodo);
     if(opcion){
-        let formulario = document.getElementById('formulario')
+        let formulario = document.getElementById('formulario');
         formulario.reset();
-        carrito = []
-        localStorage.setItem("carrito", JSON.stringify(carrito))
+        carrito = [];
+        localStorage.setItem("carrito", JSON.stringify(carrito));
         carritoStorage();
         notificacionCarrito();
-        console.log(carrito)
-        mostrarOcultarCarrito(false)
-        imprimir(productos)
+        mostrarOcultarCarrito(false);
+        imprimir(productos);
     }    
 })
-
-//---------------------------------------------------------
-/* const busqueda = productos.filter((el) => el.nombre.includes(`MOTO`));
-console.log(`Busqueda de productos que contengan MOTO en el nombre ${JSON.stringify(busqueda)}`);
-
-const valor = productos.filter((el) => el.precio <= 100000);
-console.log(`Productos de menos de $100000 de costo ${JSON.stringify(valor)}`);
-
-valor.forEach((el) => {console.log(el.nombre , el.precio)})
-
-const faltantes = productos.filter((el) => el.stock == false);
-console.log(`faltante ${JSON.stringify(faltantes)}`)
-
-*/
--- js/variables.js
+++ js/variables.js
@@ -17,19 +17,23 @@ let celulares = document.getElementById('celulares')
 let notebook = document.getElementById('notebook')
 let smartwatch = document.getElementById('smartwatch')
 let tituloSeccionDeProducos = document.getElementById('tituloSeccionDeProducos')
-let metodoDePago = document.getElementById('metodoDePago')  
+let metodoDePago = document.getElementById('metodoDePago')
+let finalCompra = document.getElementById('finalCompra');
 let productos = [];
 let carrito = [];
 let dolar = [];
+let productosALaVista = [];
+let totalEnPesos = 0;
+let totalEnDolar = 0;
 
 class Producto {
-    constructor(id, nombre, precio, categoria, stock = true){
+    constructor(id, nombre, precio, categoria, imagen){
         this.id = parseInt(id);
         this.nombre = nombre.toUpperCase();
         this.precio = parseFloat(precio).toFixed(2);
         this.categoria = categoria.toUpperCase();
         this.precioIva = parseFloat(precio * 1.21);
-        this.stock = stock;
+        this.img = imagen;
         this.carrito = parseInt(0);
     }
     sumarIva() {
@@ -40,9 +44,15 @@ class Producto {
     }
 }
 
-productos.push(new Producto(1, "Moto G60", 50000, "CELULAR"));
-productos.push(new Producto(2, "A22", 45000, "CELULAR"));
-productos.push(new Producto(3, "Moto G100", 89000, "CELULAR"));
-productos.push(new Producto(4, "Notebook Lenovo", 195000, "NOTEBOOK"));
-productos.push(new Producto(5, "LG80", 45000, "SMARTWATCH"));
-productos.push(new Producto(6, "IPHONE 13", 300000, "CELULAR"));
+productos.push(new Producto(1, "Moto G60", 50000, "CELULAR", './img/motoG60.png'));
+productos.push(new Producto(2, "A22", 49000, "CELULAR", './img/samsungA22.jpg'));
+productos.push(new Producto(3, "Moto G100", 89000, "CELULAR", './img/motoG100.png'));
+productos.push(new Producto(4, "Notebook Lenovo", 132000, "NOTEBOOK", './img/lenovoIdeapad3.jpg'));
+productos.push(new Producto(5, "Xiaomi Amazfit", 11000, "SMARTWATCH", './img/xiaomiAmazfit.jpg'));
+productos.push(new Producto(6, "IPHONE 13", 300000, "CELULAR", './img/iphone13.jpg'));
+productos.push(new Producto(7, "IPHONE 12", 250000, "CELULAR", './img/iphone12.jpg'));
+productos.push(new Producto(8, "Notebook Acer Nitro5", 240000, "NOTEBOOK", './img/acerNitro5.jpg'));
+productos.push(new Producto(9, "A52", 72000, "CELULAR", './img/samsungA52.jpg'));
+productos.push(new Producto(10, "Xiaomi Mi Band6", 6400, "SMARTWATCH", './img/xiaomiMiBand6.jpg'));
+productos.push(new Producto(11, "Notebook HP15", 125000, "NOTEBOOK", './img/hp15.jpg'));
+productos.push(new Producto(12, "Xiaomi Mi Watch2", 13000, "SMARTWATCH", './img/xiaomiMiWatch2.jpg'));
